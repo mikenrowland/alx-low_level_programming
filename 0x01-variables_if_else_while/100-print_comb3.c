f5d021189aec8d5649d7f794cf6eed990fa157ff
mykie88 <mykhelmyers@gmail.com>
2022-06-03 14:24:29 -0700

    programs for variables and conditional statements

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints all possible different
+ * combinations of two digits using putchar.The combinations
+ * are separated by a comma followed by a space, must be
+ * different digits with no repeating combinations of the same
+ * pair and prints only the smallest combination of each pair
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 1; num2 < 10; num2++)
+		{
+			if (num1 == num2)
+			{
+				continue;
+			}
+			if ((num1 - num2) == 1 || (num1 - num2) == 2)
+			{
+				continue;
+			}
+			putchar(num1 + '0');
+			putchar(num2 + '0 ');
+			if (num1 != 8 && num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
