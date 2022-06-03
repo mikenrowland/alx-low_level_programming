00304d50a0c389d69cfc24696925e70c23bbc2ce
mykie88 <mykhelmyers@gmail.com>
2022-06-02 17:54:31 -0700

    low level programming codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+//Using puts to write to stdout
 int main(void)
 {
-	char string[] = "Programming is like building a multilingual puzzle";
+	char string[] = "\"Programming is like building a multilingual puzzle";
 	puts(string);
-	return 0;
+
+	return (0);
 }
