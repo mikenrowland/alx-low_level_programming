51b56f1c70257bd78356bf540f45705051af93fc
mykie88 <mykhelmyers@gmail.com>
2022-06-03 14:36:23 -0700

    programs for variables and conditional statements

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -28,11 +28,12 @@ int main(void)
 			}
 			putchar(num1 + '0');
 			putchar(num2 + '0');
-			if ((num1 + num2) != 17)
+			if ((num1 + num2) == 17)
 			{
-				putchar(',');
-				putchar(' ');
+				break;
 			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
