a98d414b2431e9a77601a7753e16acbae44da3ea
mykie88 <mykhelmyers@gmail.com>
2022-06-03 10:20:09 -0700

    programs for variables and conditional statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point
  *
@@ -15,11 +16,11 @@ int main(void)
 
 	printf("Last digit of %d is %d", n, n % 10);
 	if (n % 10 > 5)
-		printf(" and is greater than 5");
+		printf(" and is greater than 5\n");
 	if (n % 10 == 0)
-		printf(" and is 0");
+		printf(" and is 0\n");
 	if (n % 10 < 6 && n % 10 != 0)
-		printf(" and is less than 6 and not 0");
+		printf(" and is less than 6 and not 0\n");
 
 	return (0);
 }
