449d0fc00fefc91387c7ce80bf879237e931f0e3
mykie88 <mykhelmyers@gmail.com>
2022-06-02 18:19:18 -0700

    low level programming codes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*printing to the stdout using printf*/
+	printf("with proper grammer, but the outcome is a piece of art,");
+
+	return (0);
+}
