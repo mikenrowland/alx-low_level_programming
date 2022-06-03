8ddd62950ee1aef6c5722524291a790a25939874
mykie88 <mykhelmyers@gmail.com>
2022-06-03 12:07:53 -0700

    programs for variables and conditional statements

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (; num < 10;)
+		putchar(num);
+		num++;
+	putchar('\n');
+
+	return (0);
+}
