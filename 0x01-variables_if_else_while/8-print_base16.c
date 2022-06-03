3c2d02ba35b3f69cce098c05402118210aeea9e1
mykie88 <mykhelmyers@gmail.com>
2022-06-03 12:48:48 -0700

    programs for variables and conditional statements

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program thats prints all the
+ * of base 16 in lowercase using only putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num = 0;
+	char alpha = 'a';
+
+	for (; num < 10;)
+	{
+		putchar(num);
+		num++;
+	}
+	for (; alpha <= 'f';)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
