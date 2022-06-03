f9f6bb97170ca94a3aa6d6acd43957f033a83c39
mykie88 <mykhelmyers@gmail.com>
2022-06-02 18:37:30 -0700

    low level programming codes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%d\n", sizeof(int));
+
+	printf("%d\n", sizeof(float));
+
+	printf("%d\n", sizeof(double));
+
+	printf("%d\n", sizeof(char));
+
+	printf("%d\n", sizeof(long));
+
+	return (0);
+}
