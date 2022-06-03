64ac3044384c80f849abe50fe66054b2d05fdb5e
mykie88 <mykhelmyers@gmail.com>
2022-06-03 10:36:27 -0700

    programs for variables and conditional statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = "a";
+
+	for (; alpha <= "z";)
+		putchar(alpha);
+		alpha++;
+	putchar("\n");
+
+	return (0);
+}
