5cb8bbbcf35ef2c61fe5979756d6c92b2e3c7923
mykie88 <mykhelmyers@gmail.com>
2022-06-03 15:41:50 -0700

    programs for variables and conditional statements

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,11 +19,11 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = num1; num2 < 10; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
-			for (num3 = num2; num3 < 10; num3++)
+			for (num3 = 0; num3 < 10; num3++)
 			{
-				for (num4 = num3 + 1; num4 < 10; num4++)
+				for (num4 = 1; num4 < 10; num4++)
 				{
 					putchar(num1 + '0');
 					putchar(num2 + '0');
