f8ce0a737d3895a0caf32c2a9d8327126b2c13a5
mykie88 <mykhelmyers@gmail.com>
2022-06-03 15:37:55 -0700

    programs for variables and conditional statements

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints all possible combinations
+ * of two two-digit numbers. The numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits. 1 should be printed as 01
+ * The combination of numbers must be separated by comma, followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = num1; num2 < 10; num2++)
+		{
+			for (num3 = num2; num3 < 10; num3++)
+			{
+				for (num4 = num3 + 1; num4 < 10; num4++)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
