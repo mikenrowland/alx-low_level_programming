f210d8663f1025c9ee95e812d340b3f964762233
mykie88 <mykhelmyers@gmail.com>
2022-06-07 16:35:10 -0700

    demonstrating functions and nested loops in C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main"
+
+/**
+ * print_sign - Entry point for function
+ * @n: function parameter
+ *
+ * Desc: a function that prints the sign of a number.
+ * Return: 1 if > 0, 0 if zero, otherwise -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
