48e11b4e3cb78edb5e332d4548c617bcc60c4682
mykie88 <mykhelmyers@gmail.com>
2022-06-07 05:41:07 -0700

    demonstrating functions and nested loops in C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-	int c;
-	char str[] = "_putchar";
-
-	for (c = 0; str[c] != '\0'; c++)
-	{
-		_putchar(str[c]);
-	}
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
 
 	return (0);
