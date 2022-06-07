d88140874e04ddd7ccdf88e0adfe032a4e8baae9
mykie88 <mykhelmyers@gmail.com>
2022-06-07 15:16:08 -0700

    demonstrating functions and nested loops in C

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point for function
+ * Desc:  function that checks for lowercase character.
+ * Return: 1 if lowercase or 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c < 97 || c > 122)
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
