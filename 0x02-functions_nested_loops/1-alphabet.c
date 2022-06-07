286ff3021d9ff71945a5e7ea8e30c1635e9fed6b
mykie88 <mykhelmyers@gmail.com>
2022-06-07 10:14:29 -0700

    demonstrating functions and nested loops in C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: a function that prints the alphabet,
+ * in lowercase, followed by a new line using _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar(print_alphabet());
+	_putchar('\n');
+
+	return (0);
+}
