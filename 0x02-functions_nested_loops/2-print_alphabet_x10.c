06ddfee99efed90f3f2f76103be0356720f3f0fb
mykie88 <mykhelmyers@gmail.com>
2022-06-07 14:37:54 -0700

    demonstrating functions and nested loops in C

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,11 @@
 void print_alphabet_x10(void)
 {
 	int count;
-	char alpha = 'a';
 
 	for (count = 0; count < 10; count++)
 	{
+		char alpha = 'a';
+
 		for (; alpha <= 'z';)
 		{
 			_putchar(alpha);
