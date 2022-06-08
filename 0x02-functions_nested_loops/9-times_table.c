51e221ef88fd3e575727c31f2ceeb96fea4b4e50
mykie88 <mykhelmyers@gmail.com>
2022-06-07 19:55:26 -0700

    demonstrating functions and nested loops in C

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point for function
+ * Desc: a function that prints the 9 times table, starting with 0
+ *
+ * Return: Void
+ */
+void times_table(void)
+{
+	int count;
+	int multiplier;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (multiplier = 0; multiplier < 10; multiplier++)
+		{
+			int res = count * multipler;
+
+			_putchar(res + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
