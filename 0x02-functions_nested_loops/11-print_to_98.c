6407be6d26171b67aac997a1c509d63d821e086c
mykie88 <mykhelmyers@gmail.com>
2022-06-07 20:46:08 -0700

    demonstrating functions and nested loops in C

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * print_to_98 - main function
+ * @n: parameter
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	for (; n <= 98;)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	printf('\n');
+}
