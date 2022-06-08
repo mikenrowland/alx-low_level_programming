cef7be3a3f48782ddaa49d6612aa69509a13b876
mykie88 <mykhelmyers@gmail.com>
2022-06-08 16:23:40 -0700

    demonstrating functions and nested loops in C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,7 +28,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(res + '0');
 				}
-				else if (res > 10 && res < 100)
+				else if (res >= 10 && res < 100)
 				{
 					_putchar(' ');
 					_putchar((res / 10) + '0');
