0b0f6f9c0427c2d1a2bb839d4608357ff3806540
mykie88 <mykhelmyers@gmail.com>
2022-06-07 17:03:20 -0700

    demonstrating functions and nested loops in C

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,22 +2,22 @@
 
 /**
  * _abs - Entry point for function
- * @int: parameter type
+ * @n: parameter type
  *
  * Desc: a function that computes the absolute value of an integer
  * Return: Always 0 (Success)
  */
-int _abs(int)
+int _abs(int n)
 {
-	if (int < 0)
+	if (n < 0)
 	{
-		int abs = 0 - int;
+		int abs = 0 - n;
 
 		_putchar(abs + '0');
 	}
 	else
 	{
-		_putchar(int + '0');
+		_putchar(n + '0');
 	}
 
 	return (0);
