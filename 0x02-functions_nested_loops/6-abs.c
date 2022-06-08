d337e44320a0c2ea471008a547b8f55b60dc8e22
mykie88 <mykhelmyers@gmail.com>
2022-06-07 17:00:47 -0700

    demonstrating functions and nested loops in C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point for function
+ * @int: parameter type
+ *
+ * Desc: a function that computes the absolute value of an integer
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int abs = 0 - int;
+
+		_putchar(abs + '0');
+	}
+	else
+	{
+		_putchar(int + '0');
+	}
+
+	return (0);
+}
