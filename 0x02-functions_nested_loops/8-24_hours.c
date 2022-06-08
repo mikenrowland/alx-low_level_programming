db954ac06b1fd2cfba5dacf11629685c9586a17d
mykie88 <mykhelmyers@gmail.com>
2022-06-07 21:13:52 -0700

    demonstrating functions and nested loops in C

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,19 +14,23 @@ void jack_bauer(void)
 	int num3;
 	int num4;
 
-	for (num1 = 0; num1 < 3; num1++)
+	for (num1 = 48; num1 < 51; num1++)
 	{
-		for (num2 = 0; num2 < 4; num2++)
+		for (num2 = 48; num2 < 58; num2++)
 		{
-			for (num3 = 0; num3 < 6; num3++)
+			for (num3 = 48; num3 < 54; num3++)
 			{
-				for (num4 = 0; num4 < 10; num4++)
+				for (num4 = 48; num4 < 58; num4++)
 				{
-					_putchar(num1 + '0');
-					_putchar(num2 + '0');
+					if (num1 >= 50 && num2 >= 52)
+					{
+						break;
+					}
+					_putchar(num1);
+					_putchar(num2);
 					_putchar(':');
-					_putchar(num3 + '0');
-					_putchar(num4 + '0');
+					_putchar(num3);
+					_putchar(num4);
 					_putchar('\n');
 				}
 			}
