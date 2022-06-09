d32e76cb1747775fc2170052fedeed1a693e6e5e
mykie88 <mykhelmyers@gmail.com>
2022-06-09 14:34:38 -0700

    demonstrating more functions and more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num1 < 11; num1++)
+	for (num1 = 0; num1 <= 10; num1++)
 	{
-		for (num2 = 0; num2 < 15; num2++)
+		for (num2 = 0; num2 <= 14; num2++)
 		{
-			if (num2 > 9)
+			if (num2 >= 10)
 				_putchar((num2 / 10) + '0');
 			_putchar((num2 % 10) + '0');
 		}
