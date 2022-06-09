9209aaa73dfa3d2e1678bf1ce224923e4915f98a
mykie88 <mykhelmyers@gmail.com>
2022-06-09 13:30:29 -0700

    demonstrating more functions and more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function entry point
+ * @a: function param
+ * @b: function param
+ * Desc: multiplies two ints
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
