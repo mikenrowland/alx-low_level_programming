80c18f03fd3751da80a1f30ea16b55f00f68d0f7
mykie88 <mykhelmyers@gmail.com>
2022-06-08 21:14:11 -0700

    demonstrating functions and nested loops in C

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: a program that prints the first 50 Fibonacci
+ * numbers, starting with 1 and 2, followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int count;
+int num1 = 0;
+int num2 = 1;
+int sum = 0;
+
+for (count = 0; count < 50; count++)
+{
+	sum = num1 + num2;
+	num1 = num2;
+	num2 = sum;
+	printf("%d, ", sum);
+}
+printf("\n");
+}
