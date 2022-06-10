00efaa3c8515cc7214862fc179cbc872206aad6a
mykie88 <mykhelmyers@gmail.com>
2022-06-09 21:00:12 -0700

    demonstrating more functions and more nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,12 +11,11 @@ void print_line(int n);
 {
 	int i;
 
-	if (n > 0)
+	i = 0;
+	while (i < n)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
+		i++;
 	}
 	_putchar('\n');
 }
