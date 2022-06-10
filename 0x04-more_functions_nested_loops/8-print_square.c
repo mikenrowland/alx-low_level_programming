a7ad8852c3001f646908375d18004aa00e53eee5
mykie88 <mykhelmyers@gmail.com>
2022-06-09 19:42:22 -0700

    demonstrating more functions and more nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - function entry point
+ * @size: function param
+ * Desc: a function that prints a square, followed by a new line
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int incr;
+	int squ;
+
+	if (size <= 0)
+		putchar('\n');
+	for (incr = 0; incr < size; incr++)
+	{
+		for (squ = 0; squ < size; squ++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
