a013007d5f740fd8ee3efd7f5026b806e8eff195
mykie88 <mykhelmyers@gmail.com>
2022-06-13 14:41:05 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function entry point
+ * @*n: function param
+ * Desc: a function that takes a pointer as as a parameter
+ * and updates the value it points to
+ * Return: void
+ * */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05-pointers_arrays_strings
+
+Using functions to demonstrate pointers, arrays and strings.
