6e30807175084c381bb71e501e997e453dfc7357
mykie88 <mykhelmyers@gmail.com>
2022-06-13 17:17:24 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function entry point
+ * @s: function param
+ * Desc: prints a string in reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+	int y;
+	
+	while (s[0] != '\0')
+	{
+		s++;
+		x++;
+	}
+
+	for (y = x; y >= 0; y--)
+	{
+		_putchar(s[y]);
+	}
+	_putchar('\n');
+}
