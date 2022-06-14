a29e0ed1c2d4223d2fd079fc66c5f5740bbbfd3d
mykie88 <mykhelmyers@gmail.com>
2022-06-13 17:48:02 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - function entry point
+ * @s: function param
+ * Desc: reverses a string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len;
+	int x = 0;
+	char rev;
+	
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (x = len - 1; x >= len / 2; x--)
+	{
+		rev = s[x];
+		s[x] = s[len - x - 1];
+		s[len - x - 1] = rev;
+	}
+}
