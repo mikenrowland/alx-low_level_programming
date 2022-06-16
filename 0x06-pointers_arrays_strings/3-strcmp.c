b7a6932d126400afa00ffb40871445d0fa2be931
mykie88 <mykhelmyers@gmail.com>
2022-06-16 16:10:03 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int x = 0;
 
-	for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
+	for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
 	{
 		if (s1[x] != s2[x])
 		{
