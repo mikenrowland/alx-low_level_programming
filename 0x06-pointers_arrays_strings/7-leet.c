00a1b434f73749e608244904c2b2f4e3534227c2
mykie88 <mykhelmyers@gmail.com>
2022-06-16 15:15:17 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,17 +12,17 @@ char *leet(char *s)
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		if (s[x] == 65 || s[x] == 97)
+		while (s[x] == 65 || s[x] == 97)
 		{
 			*(s + x) = '4';
 		}
-		else if (s[x] == 69 || s[x] == 101)
+		while (s[x] == 69 || s[x] == 101)
 			*(s + x) = '3';
-		else if (s[x] == 79 || s[x] == 111)
+		while (s[x] == 79 || s[x] == 111)
 			*(s + x) = '0';
-		else if (s[x] == 84 || s[x] == 116)
+		while (s[x] == 84 || s[x] == 116)
 			*(s + x) = '7';
-		else if (s[x] == 76 || s[x] == 108)
+		while (s[x] == 76 || s[x] == 108)
 			*(s + x) = '1';
 	}
 
