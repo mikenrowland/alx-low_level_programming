1e31741f07dae0650bb4e679a349b351b16b6c59
mykie88 <mykhelmyers@gmail.com>
2022-06-16 07:28:47 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * string_toupper - function entry point
+ * @*: pointer variable
+ * Desc: changes all lowercase letters of a string to uppercase
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int x = 0;
+	int len = 0;
+
+	while (s[x])
+	{
+		len++;
+		x++;
+	}
+
+	for (x = 0; x < len; x++)
+	{
+		if (s[x] > 96 && s[x] < 123)
+		{
+			s[x] = s[x] - 32;
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -2,30 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (i != 0)
-		{
-			printf(", ");
-		}
-		printf("%d", a[i]);
-		i++;
-	}
-	printf("\n");
-}
-
 int main(void)
 {
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	char str[] = "Look up!\n";
+	char *ptr;
 
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
 
 	return (0);
 }
