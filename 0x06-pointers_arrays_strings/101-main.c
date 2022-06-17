b313f8c0798dd524d7b8c15db6c7ce0e3f0bed91
mykie88 <mykhelmyers@gmail.com>
2022-06-16 19:00:40 -0700

    More pointers, strings and arrays

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main()
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - function entry point
+ * @n: function param
+ * Desc: prints and integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = 0 - n;
+	}
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
