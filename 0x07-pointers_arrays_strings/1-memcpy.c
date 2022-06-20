d0bed6c99528ed7af9b084a0d8a52b6412d4e099
mykie88 <mykhelmyers@gmail.com>
2022-06-20 12:18:26 -0700

    memcpy clone using pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - function entry point
+ * @dest: destination param
+ * @src: source param
+ * @n: number of bytes
+ * Desc: copies n bytes from memory area src to memory area dest
+ *
+ * Return: string pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		*dest = src[x];
+		dest++;
+	}
+
+	return (dest);
+}
