561ae460870d2ff32093e62dc02e1ff298774433
mykie88 <mykhelmyers@gmail.com>
2022-06-20 17:44:45 -0700

    strstr clone using pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strstr - function entry point
+ * @haystack: param
+ * @needle: param
+ * Desc: finds the first occurrence of the substring
+ * needle in the string haystack. The terminating null
+ * bytes (\0) are not compared
+ *
+ * Return: pointer to the beginning of the located
+ * substring, or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x;
+
+	while (haystack)
+	{
+		x = 0;
+
+		if (haystack[x] == needle[x])
+		{
+			do {
+				if (needle[x + 1] != '\0')
+				{
+					x++;
+				}
+				else
+				{
+					return (haystack);
+				}
+			} while (haystacl[x] == needle[x]);
+		}
+		haystack++;
+	}
+
+	return (NULL);
+}
