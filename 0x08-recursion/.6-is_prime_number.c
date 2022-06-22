5268a94b61cca5750d1eb8b91db11a1a78bfbbfa
mykie88 <mykhelmyers@gmail.com>
2022-06-22 16:08:38 -0700

    checks if an int is a prime number using recursion

---
+++ 0x08-recursion/.6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function entry point
+ * @n: param
+ * Desc: checks if int n is a prime number
+ *
+ * Return: returns 1 if the input integer is a prime number, else 0
+ */
+int is_prime_number(int n)
+{
+	if (z <= 1)
+	{
+		return (0);
+	}
+	return(find_prime());
+}
+
+/**
+ * find_prime - function entry point
+ * @x: param
+ * @y: param
+ * @z: param
+ * Desc: finds a prime number
+ *
+ * Return: 1 if prime is found, else 0
+ */
+int find_prime(int x, int y, int z)
+{
+	return(find_prime());
+}
