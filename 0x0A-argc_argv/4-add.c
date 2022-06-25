e1be173e7b8c65beb14cf05653429424ad0ee0db
mykie88 <mykhelmyers@gmail.com>
2022-06-24 19:21:14 -0700

    addition operation on ints from command line

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function entry point
+ * @argc: args count
+ * @argv: command line args
+ * Desc: adds positive numbers and prints error if args is non-digit
+ *
+ * Return: Always 0 or 1 if an error occurs
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int x;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (x = 1; x < argc; x++)
+	{
+		if (atoi(argv[x]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[x]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
