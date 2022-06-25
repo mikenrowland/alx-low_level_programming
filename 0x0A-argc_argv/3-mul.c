86b1c7c07818a5fa4a204185d3c62724d7a3f2c5
mykie88 <mykhelmyers@gmail.com>
2022-06-24 18:53:40 -0700

    multiplication on command line args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function entry point
+ * @argc: number of args
+ * @argv: command line args
+ * Desc: multiplies two numbers passed as argv
+ *
+ * Return: result of the multiplication or 1 if an error occurred
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 2 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return(0);
+}
