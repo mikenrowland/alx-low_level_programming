c56d54165816f8147b7983a0e98b6fca87a120c6
mykie88 <mykhelmyers@gmail.com>
2022-06-29 10:00:53 -0700

    dynamic memory allocation with malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int _strlen(char *s);
+/**
+ * argstostr - function entry point
+ * @ac: function param int
+ * @av: function param str
+ * Desc:  concatenates all the arguments of a program
+ *
+ * Return: ptr to new str or NULL if av is NULL || ac == 0
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int x, y, z, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	x = 0;
+	while (x < ac)
+	{
+		len += _strlen(av[x]);
+		x++;
+	}
+
+	s = malloc(sizeof(char) * (len * 2 + 1));
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+	x = 0;
+	y = 0;
+	while (x < ac)
+	{
+		z = 0;
+		while (av[x][z])
+		{
+			s[y] = av[x][z];
+			y++;
+			z++;
+		}
+		s[y] = '\n';
+		y++;
+		x++;
+	}
+	s[y] = '\0';
+
+	return (s);
+}
+
+/**
+ * _strlen - function entry point
+ * @s: function param
+ * Desc: a function that returns the len of a string
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
---
+++ 0x0B-malloc_free/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char *av[])
+{
+	char *s;
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
+}
