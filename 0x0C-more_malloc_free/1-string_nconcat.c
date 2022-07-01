a649d1290c092c7522a486e8ddc18361d539b199
mykie88 <mykhelmyers@gmail.com>
2022-06-30 18:54:56 -0700

    Dynamic memory allocation with malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	int x, y, len = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	s = malloc(sizeof(s1) + n);
 	if (s == NULL)
 		return (NULL);
