36d86b905728cc6f7ce6258de05657ba54d6849b
mykie88 <mykhelmyers@gmail.com>
2022-06-30 20:17:30 -0700

    Dynamic memory allocation with malloc and free

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function entry point
+ * @min: param
+ * @max: param
+ * Desc: creates an array of integers containing all
+ * the values from min (included) to max (included),
+ * ordered from min to max
+ *
+ * Return: pointer to the newly created array or NULL
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int x;
+
+	if (min > max)
+		return (NULL);
+	arr = malloc(sizeof(int) * (min + max));
+	if (arr == NULL)
+		return (NULL);
+	x = min;
+	while (x < max + 1)
+	{
+		arr[x] = x;
+		x++;
+	}
+	return (arr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,3 +4,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
