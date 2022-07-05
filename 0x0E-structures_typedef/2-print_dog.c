8ee05e86ec6a19622a2bef23abed0f8bf4797046
mykie88 <mykhelmyers@gmail.com>
2022-07-04 21:19:53 -0700

    freed dogs

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,13 +14,16 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("Name: (nil)");
-		printf("Name: %s\n", d->name);
+			printf("Name: (nil)\n");
+		elsep
+			rintf("Name: %s\n", d->name);
 		if (d->age < 0)
-			printf("Age: (nil)");
-		printf("Age: %f\n", d->age);
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
-			printf("Owner: (nil)");
-		printf("Owner: %s\n", d->owner);
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function entry
+ * @name: str
+ * @age: float
+ * @owner: str
+ * Desc: creates a new dog
+ *
+ * Return: ptr to new dog or NULL on fail
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	/*pass to be continued*/
+	return (NULL);
+}
