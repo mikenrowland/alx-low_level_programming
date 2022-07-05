e8cbe698a0a6ac065156d135aeac3c2b3a945c62
mykie88 <mykhelmyers@gmail.com>
2022-07-04 17:41:54 -0700

    removed extra space in macro identifier

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H 
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,5 @@
 #ifndef PI_H
-#define PI_H 
+#define PI_H
 
 #define PI 3.14159265359
 
