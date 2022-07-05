e2b480125c51720bdc3cefaea0f43c8aae885115
mykie88 <mykhelmyers@gmail.com>
2022-07-04 20:38:36 -0700

    updated condition for age element

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,16 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
-		printf("Name: (nil)");
-	printf("Name: %s\n", d->name);
-	if (!(d->age))
-		printf("Age: (nil)");
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: (nil)");
-	printf("Owner: %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)");
+		printf("Name: %s\n", d->name);
+		if (d->age < 0)
+			printf("Age: (nil)");
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner: (nil)");
+		printf("Owner: %s\n", d->owner);
+	}
 }
