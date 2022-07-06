b624abf0b5bf1cc2a36290f004053a74d0019b03
mykie88 <mykhelmyers@gmail.com>
2022-07-06 12:02:51 -0700

    print name using function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - funtion entry point
+ * @name: str to print
+ * @f: function pointer
+ * Desc: a function that prints a name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+# 0x0F. Function pointers in C
