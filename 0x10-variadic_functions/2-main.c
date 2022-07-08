c7e03afebc736a361fd7c99c9c016257708b796d
mykie88 <mykhelmyers@gmail.com>
2022-07-07 20:14:50 -0700

    fixed requirement for null string

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,7 +1,8 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
 
 int main(void)
 {
-	print_strings(", ", 2, "Jay", "Django");
+	print_strings(", ", 3, "Jay", "Django", NULL);
 	return (0);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		}
 		else
-			printf("nil");
+			printf("(nil)");
 		if (separator && i < n - 1)
 			printf("%s", separator);
 		i++;
