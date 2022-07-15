074fafa0e79227b26b8a58358290126c60114074
mykie88 <mykhelmyers@gmail.com>
2022-07-15 14:39:48 -0700

    assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+    msg db "Hello, Holberton",10
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 16
+    syscall
+    
+    mov rax, 60
+    mov rdi, 0
+    syscall
