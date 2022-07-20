a2b2006acd89c82f9d499f2fd5d78b2ca18ce161
mykie88 <mykhelmyers@gmail.com>
2022-07-19 17:31:16 -0700

    fixed output fail

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,14 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *freed;
+
 	if (head == NULL)
 		return;
 	while (*head)
 	{
+		freed = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
+		*head = freed;
 	}
 }
