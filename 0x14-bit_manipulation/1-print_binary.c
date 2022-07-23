aa325c03bc95f35f85e6acc55ed638898e3a6afe
mykie88 <mykhelmyers@gmail.com>
2022-07-23 03:33:10 -0700

    print binary from base 10

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - function entry
+ * @n: long u_int
+ * Desc: prints the binary representation of a number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	while (n >= 1)
+	{
+		print_binary(n / 2);
+		if (n % 2 == 0)
+		{
+			_putchar('0');
+			return;
+		}
+		_putchar(1 + '0');
+		return;
+	}
+	return;
+}
+
