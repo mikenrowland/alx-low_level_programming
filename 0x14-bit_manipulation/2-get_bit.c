a15d319fef5ce1fdeb8982e7cb9ab9f5fcd633d7
mykie88 <mykhelmyers@gmail.com>
2022-07-24 06:51:44 -0700

    get bit by index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_bit - function entry
+ * @n: int whose bit to get
+ * @index: position of bit
+ * Desc: a function that returns the
+ *	value of a bit at a given index
+ * Return: the value of the bit at given
+ *	index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit;
+	int x = 0;
+
+	while (n != 0)
+	{
+		bit = n & 1;
+		if (x == index)
+			return (bit);
+		n = n >> 1;
+		x++;
+	}
+	return (-1);
+}
